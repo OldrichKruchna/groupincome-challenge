e7e97d2f639344fde7f5de218686c0056c54ca5b
Oldrich Kruchna <oldrich.kruchna@sinnerschrader.com>
2019-01-06 12:25:54 +0100

    basic menu style draft

--- src/Menu.vue
+++ src/Menu.vue
@@ -1,20 +1,26 @@
 <template>
-	<div class="menu">
-		<span>User settings</span>
-		<ul>
-			<li>My account</li>
-			<li>Privacy & Safety</li>
-		</ul>
-		<span>App settings</span>
-		<ul>
-			<li>Notifications</li>
-			<li>Appearance</li>
-		</ul>
-		<hr>
-		<ul>
-			<li>Changelog</li>
-			<li>Log Out</li>
-		</ul>
+	<div class="menu__wrapper">
+		<div class="menu">
+			<div class="menu__list">
+				<span class="headline">User settings</span>
+				<ul>
+					<li>My account</li>
+					<li>Privacy & Safety</li>
+				</ul>
+			</div>
+			<div class="menu__list">
+				<span class="headline">App settings</span>
+				<ul>
+					<li>Notifications</li>
+					<li class="active">Appearance</li>
+				</ul>
+				<hr>
+				<ul>
+					<li>Changelog</li>
+					<li>Log Out</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -24,4 +30,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.menu {
+	&__wrapper {
+		display: flex;
+		justify-content: flex-end;
+		width: 400px;
+		height: 100%;
+		padding: 16px 8px;
+	}
+}
+
+span {
+	height: 32px;
+}
+
+.menu__list {
+	padding-bottom: 16px;
+}
+
+li {
+	height: 32px;
+	width: 160px;
+	display: flex;
+	align-items: center;
+	padding: 0 8px;
+	border-radius: 4px;
+
+	&.active {
+		background-color: #5DC8F0;
+		color: white;
+	}
+}
+
+hr {
+	display: block;
+	margin: 16px auto;
+	width: calc(100% - 16px);
+}
+
+.headline {
+	display: flex;
+	align-items: center;
+	padding: 0 8px;
+	text-transform: uppercase;
+	// TODO color fix
+	color: grey;
+}
 </style>
