cf27052fedb132043be50204a1c195b0414fc615
Oldrich Kruchna <oldrich.kruchna@sinnerschrader.com>
2019-01-06 11:39:53 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+	<div>
+		<button type="button" name="button" v-on:click="openModal()">Open the modal</button>
+		<Modal>Tuc</Modal>
+	</div>
+</template>
+
+<script>
+import eventBus from './eventBus';
+
+import Modal from './Modal.vue';
+
+export default {
+	data() {
+		return {
+			isModalOpen: false
+		};
+	},
+	components: {
+		Modal
+	},
+	methods: {
+		openModal() {
+			eventBus.$emit('openModal');
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+#app {
+  // font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+  // margin-top: 60px;
+}
+
+h1, h2 {
+  // font-weight: normal;
+}
+
+ul {
+  // list-style-type: none;
+  // padding: 0;
+}
+
+li {
+  // display: inline-block;
+  // margin: 0 10px;
+}
+
+a {
+  // color: #42b983;
+}
+</style>
---
+++ src/Modal.vue
@@ -0,0 +1,57 @@
+<template >
+	<div class="modal" v-if="open">
+		<slot></slot>
+		<div class="close-button" v-on:click="close()">x</div>
+	</div>
+</template>
+
+<script>
+import eventBus from './eventBus';
+
+export default {
+	data() {
+		return {
+			open: false
+		}
+	},
+	mounted() {
+    eventBus.$on('openModal', () => {
+			this.open = true;
+    });
+
+		// listen for ESC pressed
+  },
+	methods: {
+		close() {
+			this.open = false;
+		}
+	}
+};
+</script>
+
+<style style="scss" scoped>
+	.modal {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 10000;
+	}
+
+	.close-button {
+		position: absolute;
+		right: 40px;
+		top: 20px;
+		width: 40px;
+		height: 40px;
+		background-color: #ccc;
+		border-radius: 50%;
+		cursor: pointer;
+
+		/* TODO use icon instead */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
